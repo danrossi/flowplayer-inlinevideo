9740a7de2b20f1a0a0b7d1bb60484992221a2e45
Daniel Rossi <electroteque@gmail.com>
2016-07-26 23:01:45 +1000

    Updates for IOS and Android standalone pinned apps support.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 flowplayer.inlinevideo.js
 =========================
 
-This plugin generates a video tag with webkit-playsinline attribute support. This is required for Apache Cordova inline video playback support in IOS. This is completely
-missing from all Html5 based engines. A similar hack is required for crossorigin support which is also added for convenience.
+This plugin generates a video tag with webkit-playsinline attribute support. This is required for both "pin to Home Screen" IOS WebView apps and Apache Cordova inline video playback support in IOS.
+
+This is completely missing from all Html5 based engines. A similar hack is required for crossorigin support which is also added for convenience.
+
+The feature detection will override an "inlineVideo" flowplayer support config for Cordova and standalone IOS WebView apps. This is needed or video is completely hidden.
 
 Configure Cordova For Inline Video Support
 ==========================================
@@ -14,3 +17,45 @@ Configure the following tags to enable both inline video support and playback wi
   <preference name="MediaPlaybackRequiresUserAction" value="false"/>
   <preference name="MediaPlaybackAllowsAirPlay" value="true"/>
 ```
+
+Standalone App support
+======================
+
+Special metadata tags are required to be added for both Android and IOS pinned standalone app support. This includes a link to the app icon.
+If these tags are already configured then a duplicate tag will not be added.
+
+The following will be prepended to the page
+
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=no,user-scalable=no,minimal-ui">
+
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="black">
+<link rel="icon" sizes="192x192" href="/path/to/icon.png">
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="apple-touch-icon" sizes="192x192" href="/path/to/icon.png">
+
+See more:
+
+https://developer.apple.com/library/mac/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
+https://developer.chrome.com/multidevice/android/installtohomescreen
+
+Create HomeScreen app of webpage
+================================
+
+1) touch the pin icon at the bottom of Safari.
+2) touch "Add to Home Screen"
+3) choose a name and touch "Add"
+
+
+Configuration
+=============
+
+touchIcon - the url to the touch icon which will be used for the app icon.
+iconSizes - the icon dimensions ie. 192x192
+
+Known Issues
+============
+
+Only Cordova apps can hide the status bar. In IOS standalone apps there is now no way to hide the statusbar even when going fullscreen video.
--- src/flowplayer.inlinevideo.js
+++ src/flowplayer.inlinevideo.js
@@ -1,6 +1,9 @@
 /* flowplayer.inlinevideo.js
  * Inline video support for Flowplayer 6
  * This is a hack to work around limitations in all available Html5 based engines that have the inline attribute missing.
+ * Injects required metadata for inline play back support on IOS Iphone. All other mobile devices ok.
+ * This requires "pinning" the page to the home screen which creates a standalone WebView app.
+ * This static function is based on https://github.com/aframevr/aframe/blob/master/src/core/scene/metaTags.js
  * 2016-05-14
  *
  * By Daniel Rossi, Electroteque Media
@@ -18,10 +21,25 @@
 flowplayer(function(player, root) {
 
   var common = flowplayer.common,
-    video = common.find("video", root)[0];
+    video = common.find("video", root)[0],
+      UA = navigator.userAgent,
+      isIOS = /iP(hone|od|ad)/.test(UA),
+      isAndroid = /MeeGo|Android/.test(UA),
+      isMobile =  isAndroid || isIOS,
+      config = {
+        touchIcon: null,
+        iconSizes: "192x192",
+        isIOS: isIOS,
+        isMobile: isMobile
+      };
+
+  flowplayer.extend(config, player.conf.inline);
+
+  //inject metadata required for inline video support.
+  MetadataInjector(config);
 
   //if the cordova api is available enable inline video support.
-  if (!!window.cordova) flowplayer.support.inlineVideo = true;
+  if (!!window.cordova || !!window.navigator.standalone) flowplayer.support.inlineVideo = true;
 
   function setInlineVideo()  {
     if (!video) return;
@@ -51,3 +69,74 @@ flowplayer(function(player, root) {
   setInlineVideo();
 
 });
+
+/**
+ * Injects required metadata for inline play back support on IOS Iphone. All other mobile devices ok.
+ * This requires "pinning" the page to the home screen which creates a standalone WebView app.
+ * This static function is based on https://github.com/aframevr/aframe/blob/master/src/core/scene/metaTags.js
+ * @param config
+ * @constructor
+ */
+function MetadataInjector(config) {
+  function inject() {
+    var headEl = document.head;
+    var headScriptEl = headEl.querySelector('script');
+    var tag;
+    var headTags = [];
+
+    if (config.isMobile) {
+
+      ddMetaTag("viewport", 'width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=no,user-scalable=no,minimal-ui');
+      addMetaTag("mobile-web-app-capable", "yes");
+      addMetaTag("theme-color", "black");
+      if (!!config.touchIcon) addLinkTag("icon", config.touchIcon, config.iconSizes);
+
+
+
+      if (config.isIOS) {
+        addMetaTag("apple-mobile-web-app-capable", "yes");
+        addMetaTag("apple-mobile-web-app-status-bar-style", "black-translucent");
+
+        if (!!config.touchIcon) addLinkTag("apple-touch-icon", config.touchIcon, config.iconSizes);
+      }
+    }
+
+    function exists(name) {
+      return document.querySelector('meta[name="' + name + '"]');
+    }
+
+    function iconExists(rel) {
+      return document.querySelector('link[rel="' + rel + '"]');
+    }
+
+    function addMetaTag(name, content) {
+      if (!exists(name)) {
+        var meta = document.createElement('meta');
+        meta.name = name;
+        meta.content = content;
+        append(meta);
+      }
+    }
+
+    function addLinkTag(name, icon, sizes) {
+      if (!iconExists(name)) {
+        var meta = document.createElement('link');
+        meta.rel = name;
+        meta.sizes = sizes;
+        meta.href = icon;
+        append(meta);
+      }
+    }
+
+    function append(tag) {
+      if (headScriptEl) {
+        headScriptEl.parentNode.insertBefore(tag, headScriptEl);
+      } else {
+        headEl.appendChild(tag);
+      }
+    }
+
+  }
+
+  inject();
+}
